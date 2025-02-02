b723ce5f88c503436165bd8adcb3bebec593ab5a
Álvaro Hernández <alvarohcolino@gmail.com>
2025-02-02 19:22:31 +0100

    add hero section

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,17 +2,18 @@ import Image from 'next/image';
 
 export default function Header() {
   return (
-    <header className="bg-primary p-6">
+    <header className="bg-secondary-dark p-6">
       <div className="flex items-center gap-3">
         <Image
-        className='rounded-lg'
+          className="rounded-lg"
           src="oriview-logo.svg"
           alt="OriView Logo"
           width={50}
           height={50}
         />
-        <h1 className='text-2xl font-black text-text'>OriView</h1>
+        <h1 className="text-2xl font-black text-text">OriView</h1>
       </div>
+
     </header>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from '@/app/components/Header';
+import Hero from '@/app/components/Hero';
 
 export default function Home() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
