a6aae117c448ff88b7e4ef40a4fe7a35fe4a9420
Álvaro Hernández <alvarohcolino@gmail.com>
2025-02-02 19:34:02 +0100

    Add header links

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,8 +1,21 @@
+import clsx from 'clsx';
 import Image from 'next/image';
+import Link from 'next/link';
+
+type LinkT = {
+  name: string;
+  href: string;
+};
 
 export default function Header() {
+  const links: LinkT[] = [
+    { name: 'Features', href: '/features' },
+    { name: 'Events', href: '/events' },
+    { name: 'Community (soon)', href: '/community' },
+  ];
+
   return (
-    <header className="bg-secondary-dark p-6">
+    <header className="flex justify-between items-center bg-secondary-dark p-6">
       <div className="flex items-center gap-3">
         <Image
           className="rounded-lg"
@@ -14,6 +27,19 @@ export default function Header() {
         <h1 className="text-2xl font-black text-text">OriView</h1>
       </div>
 
+      <nav className="flex gap-4 items-center ">
+        {links.map((link) => (
+          <Link
+            className={`font-semibold text-text ${clsx({
+              'line-through text-text/40': link.href === '/community',
+            })}`}
+            key={link.href}
+            href={link.href}
+          >
+            {link.name}
+          </Link>
+        ))}
+      </nav>
     </header>
   );
 }
