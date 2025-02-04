481da71360950e171b4a95b4d0f08e71eb801cf9
Álvaro Hernández <alvarohcolino@gmail.com>
2025-02-04 17:49:13 +0100

    change hero section

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,7 +1,9 @@
 export default function Hero() {
-    return (
-        <section className="h-125 hero-gradient">
-            
-        </section>
-    );
-    }
+  return (
+    <section className="flex justify-center items-center h-120">
+      <h1 className="text-7xl w-1/2 text-center text-text font-black z-10">
+        What's happening in the universe?
+      </h1>
+    </section>
+  );
+}
