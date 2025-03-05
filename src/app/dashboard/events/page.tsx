0b8ac9943f92914f5b007017fdc2eabd6cbad456
Álvaro Hernández <alvarohcolino@gmail.com>
2025-03-05 13:38:01 +0100

    add basic mockup for events page

--- src/app/dashboard/events/page.tsx
+++ src/app/dashboard/events/page.tsx
@@ -1,10 +1,20 @@
-import UnderConstructionMessage from "@/app/components/ui/UnderConstruction";
+import UnderConstructionMessage from '@/app/components/ui/UnderConstruction';
 
 export default function EventsPage() {
   return (
-    <section className="flex flex-col items-center justify-center h-full">
-      {/* <h1 className="text-3xl font-bold">Events Page</h1> */}
-      <UnderConstructionMessage />
+    <section className="flex flex-col gap-10 h-full">
+      <h1 className="text-3xl font-black">Events Dashboard</h1>
+      <main className="grid grid-cols-2 gap-5 h-full">
+        <div className="bg-secondary-light text-primary">
+          <h2>Up comming events</h2>
+        </div>
+        <div className="bg-secondary-light text-primary">
+          <h2>Calendar</h2>
+        </div>
+        <div className="bg-secondary-light text-primary col-span-2">
+          <h2>Time line comming soon!</h2>
+        </div>
+      </main>
     </section>
   );
 }
