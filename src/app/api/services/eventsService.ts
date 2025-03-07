17596d91a35f5dfd2ee4cec10bf60aec24ba56bd
Álvaro Hernández <alvarohcolino@gmail.com>
2025-03-07 20:12:00 +0100

    add sorting for getUpcomingEvents service

--- src/app/api/services/eventsService.ts
+++ src/app/api/services/eventsService.ts
@@ -2,13 +2,14 @@ import axios from 'axios';
 
 const BASE_URL = 'https://www.datastro.eu/api/explore/v2.1/catalog/datasets';
 
-export default async function getUpcomingEclipses(endYear: number) {
+export async function getUpcomingEclipses(endYear: number) {
   const currentYear = new Date().getFullYear();
   const query = `calendar_year>=${currentYear} AND calendar_year<=${endYear}`;
+  const sorting = 'calendar_year, calendar_month, calendar_day';
 
   const url = `${BASE_URL}/five-millennium-catalog-of-solar-eclipses0/records?where=${encodeURIComponent(
     query
-  )}&limit=-1`;
+  )}&order_by=${encodeURIComponent(sorting)}&limit=-1`;
 
   const { data } = await axios(url);
 
